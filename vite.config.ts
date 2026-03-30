7a6b7b5a35b862899ff7fb520ce43e7a6dacc225
Skill-Aura-Official <learnfusion03@gmail.com>
2026-03-30 13:08:40 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
